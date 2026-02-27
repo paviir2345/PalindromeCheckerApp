f3ba9a8c363368da30e1aea8164c2bbdcf775a22
paviir2345 <paviir2345@gmail.com>
2026-02-27 16:28:58 +0530

    UC5: Palindrome checker using Stack data structure

--- Main.java
+++ Main.java
@@ -1,25 +1,36 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Palindrome Checker App");
+        System.out.println("Palindrome Checker using Stack");
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a word: ");
         String original = scanner.nextLine();
 
-        // Using StringBuilder to reverse
-        StringBuilder builder = new StringBuilder(original);
-        String reversed = builder.reverse().toString();
+        Stack<Character> stack = new Stack<>();
 
-        // Compare strings
-        if(original.equals(reversed)) {
-            System.out.println(original + " is a palindrome");
+        // Push characters
+        for (int i = 0; i < original.length(); i++) {
+            stack.push(original.charAt(i));
+        }
+
+        // Pop characters to reverse
+        String reversed = "";
+
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
+        }
+
+        // Compare
+        if (original.equals(reversed)) {
+            System.out.println("Palindrome");
         } else {
-            System.out.println(original + " is not a palindrome");
+            System.out.println("Not Palindrome");
         }
 
         scanner.close();
